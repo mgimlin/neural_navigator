2783b6a49ac3cae8272f40ea1b229f705da2abd2
Joseph <timothy.b.joseph@tamu.edu>
2024-03-07 16:18:26 -0600

    autodistill

---
+++ auto_label/label_with_autodistill.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+
+
+
+
+# class label_with_autodistill():
+
+
+
+def label_with_autodistill(images_path):
+    def __init__(self):
+        HOME = os.getcwd()
+    
+    
+
+
+
+
+label_with_autodistill("")
