b785e6ac70bcb2bc004aeece674dc93775dbf963
Matthew-Gimlin <matthew32602@gmail.com>
2024-04-23 18:07:27 -0500

    cleaning up code

--- 3D_interface/the_real_2d_test.py
+++ 3D_interface/the_real_2d_test.py
@@ -8,7 +8,7 @@ import cv2
 import torch
 
 model = YOLO('../../yolo/gator_results/weights/best.pt')
-cam = cv2.VideoCapture(1)
+cam = cv2.VideoCapture(0)
 if not cam.isOpened():
     exit()
 
@@ -50,7 +50,7 @@ colors = (
 import math
 
 AVERAGE_HEIGHTS = {
-    'person': 1.77
+    'person': 1.77,
 }
 
 CAMERA_HEIGHT = 1.0
@@ -70,37 +70,27 @@ def estimate_depth(y1: float, y2: float) -> float:
     """
     y_top = min(y1, y2)
     y_bottom = max(y1, y2)
-
     depth = 0.0
-
-    global colors
 
     if y_bottom > 0.99 and y_top < 0.01:
         depth = -1.0
 
     elif y_bottom > 0.99 and y_top > 0.0:
-        colors = (
-            (1, 0, 0), (0, 1, 0), (0, 0, 1),
-            (1, 1, 0), (1, 0, 1), (0, 1, 1)
-        )
         opposite = AVERAGE_HEIGHTS['person'] - CAMERA_HEIGHT
         angle = CAMERA_FOV_Y * abs(0.5 - y_top)
         depth = opposite / math.tan(math.radians(angle))
         depth *= -depth
 
     else:
-        colors = (
-            (1, 0, 1), (0, 1, 1),
-            (1, 0, 0), (0, 1, 0), (0, 0, 1),
-            (1, 1, 0),
-        )
         angle = CAMERA_FOV_Y * abs(0.5 - y_bottom)
         depth = CAMERA_HEIGHT / math.tan(math.radians(angle))
         depth *= -depth
 
     return depth
 
-def cube():
+def cube() -> None:
+    """Adds a cube to the scene.
+    """
     glBegin(GL_QUADS)
     for i, face in enumerate(faces):
         glColor3fv(colors[i])  # Set color for each face
@@ -108,13 +98,16 @@ def cube():
             glVertex3fv(vertices[vertex])
     glEnd()
 
-def display():
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+def display() -> None:
+    """
+    """
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
-    gluPerspective(90, (WIDTH/HEIGHT), 0.1, 50.0)
+    gluPerspective(90, WIDTH / HEIGHT, 0.1, 50.0)
     glTranslatef(0.0, -0.5, -5)
     glRotatef(45, 1, 0, 0)
 
+    # Get a frame from the camera.
     ret, frame = cam.read()
     if not ret:
         return
@@ -122,52 +115,36 @@ def display():
     # Object detection.
     results = model(frame)
 
-    # Depth estimation.
-    # img = frame
-    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # input_batch = transform(img).to(device)
-    # with torch.no_grad():
-    #     prediction = midas(input_batch)
-    #     prediction = torch.nn.functional.interpolate(
-    #         prediction.unsqueeze(1),
-    #         size=img.shape[:2],
-    #         mode="bicubic",
-    #         align_corners=False,
-    #     ).squeeze()
-    # depth_map = prediction.cpu().numpy()
-
     for result in results:
         for box in result.boxes.xyxyn:
-            center_x = (box[0] + box[2]) / 2
-            center_y = (box[1] + box[3]) / 2
-
-            # print(center_x, len(depth_map), int(center_x * len(depth_map[0])))
-            # depth = depth_map[int(center_x * len(depth_map))][int(center_y * len(depth_map[0]))]
-
+            # Estimate the distance from the camera to the object.
             depth = estimate_depth(box[1], box[3])
 
+            # Scale the x position depending on the depth.
+            center_x = (box[0] + box[2]) / 2
             angle = CAMERA_FOV_X * abs(0.5 - center_x)
 
             glPushMatrix()
             glScalef(0.25, 0.25, 0.25)
             glTranslatef(
-                # 20 * center_x - 10,
                 depth * math.tan(math.radians(angle)),
                 -0.5,
-                # -5, # No depth.
-                # 0 - 10 * (1 - abs(box[1] - box[3])) # Rough relative depth.
-                10 + depth # MiDaS relative depth.
+                10 + depth
             )
             cube()
             glPopMatrix()
 
     glutSwapBuffers()
 
-def update(value):
+def update(value: int) -> None:
+    """
+    """
     glutPostRedisplay()
     glutTimerFunc(1000 // 60, update, 0)
 
-def main():
+def main() -> None:
+    """
+    """
     glutInit()
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
     glutInitWindowSize(WIDTH, HEIGHT)
