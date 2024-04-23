97ac94b598f37352d251d7ed49ddc141c018ca66
Matthew-Gimlin <matthew32602@gmail.com>
2024-04-23 17:45:54 -0500

    bruh

--- 3D_interface/the_real_2d_test.py
+++ 3D_interface/the_real_2d_test.py
@@ -7,11 +7,11 @@ import cv2
 
 import torch
 
-model = YOLO('../../yolo/best.pt')
-cam = cv2.VideoCapture(0) 
+model = YOLO('../../yolo/gator_results/weights/best.pt')
+cam = cv2.VideoCapture(1)
 if not cam.isOpened():
     exit()
-    
+
 model_type = "MiDaS_small"
 midas = torch.hub.load("intel-isl/MiDaS", model_type)
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
@@ -47,7 +47,58 @@ colors = (
     (1, 1, 0), (1, 0, 1), (0, 1, 1)
 )
 
-angle = 0  # Initial angle
+import math
+
+AVERAGE_HEIGHTS = {
+    'person': 1.77
+}
+
+CAMERA_HEIGHT = 1.0
+CAMERA_FOV_X = 90
+CAMERA_FOV_Y = 50
+CAMERA_TILT = 0
+
+def estimate_depth(y1: float, y2: float) -> float:
+    """Estimates depth given the height of a bounding box and the camera parameters.
+
+    Args:
+        y1: A normalized y value of the bounding box.
+        y2: A normalized y value of the bounding box.
+
+    Returns:
+        The estimated distance from the camera to the bounding box.
+    """
+    y_top = min(y1, y2)
+    y_bottom = max(y1, y2)
+
+    depth = 0.0
+
+    global colors
+
+    if y_bottom > 0.99 and y_top < 0.01:
+        depth = -1.0
+
+    elif y_bottom > 0.99 and y_top > 0.0:
+        colors = (
+            (1, 0, 0), (0, 1, 0), (0, 0, 1),
+            (1, 1, 0), (1, 0, 1), (0, 1, 1)
+        )
+        opposite = AVERAGE_HEIGHTS['person'] - CAMERA_HEIGHT
+        angle = CAMERA_FOV_Y * abs(0.5 - y_top)
+        depth = opposite / math.tan(math.radians(angle))
+        depth *= -depth
+
+    else:
+        colors = (
+            (1, 0, 1), (0, 1, 1),
+            (1, 0, 0), (0, 1, 0), (0, 0, 1),
+            (1, 1, 0),
+        )
+        angle = CAMERA_FOV_Y * abs(0.5 - y_bottom)
+        depth = CAMERA_HEIGHT / math.tan(math.radians(angle))
+        depth *= -depth
+
+    return depth
 
 def cube():
     glBegin(GL_QUADS)
@@ -58,7 +109,6 @@ def cube():
     glEnd()
 
 def display():
-    global angle
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
     gluPerspective(90, (WIDTH/HEIGHT), 0.1, 50.0)
@@ -71,67 +121,60 @@ def display():
 
     # Object detection.
     results = model(frame)
-    
+
     # Depth estimation.
-    img = frame
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    input_batch = transform(img).to(device)
-    with torch.no_grad():
-        prediction = midas(input_batch)
-        prediction = torch.nn.functional.interpolate(
-            prediction.unsqueeze(1),
-            size=img.shape[:2],
-            mode="bicubic",
-            align_corners=False,
-        ).squeeze()
-    depth_map = prediction.cpu().numpy()
+    # img = frame
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # input_batch = transform(img).to(device)
+    # with torch.no_grad():
+    #     prediction = midas(input_batch)
+    #     prediction = torch.nn.functional.interpolate(
+    #         prediction.unsqueeze(1),
+    #         size=img.shape[:2],
+    #         mode="bicubic",
+    #         align_corners=False,
+    #     ).squeeze()
+    # depth_map = prediction.cpu().numpy()
 
     for result in results:
         for box in result.boxes.xyxyn:
             center_x = (box[0] + box[2]) / 2
             center_y = (box[1] + box[3]) / 2
 
-            # print(center_x, len(depth_map[0]), int(center_x * len(depth_map[0])))
-            depth = depth_map[int(center_x * len(depth_map))][int(center_y * len(depth_map[0]))]
-            
+            # print(center_x, len(depth_map), int(center_x * len(depth_map[0])))
+            # depth = depth_map[int(center_x * len(depth_map))][int(center_y * len(depth_map[0]))]
+
+            depth = estimate_depth(box[1], box[3])
+
+            angle = CAMERA_FOV_X * abs(0.5 - center_x)
+
             glPushMatrix()
+            glScalef(0.25, 0.25, 0.25)
             glTranslatef(
-                20 * center_x - 10,
+                # 20 * center_x - 10,
+                depth * math.tan(math.radians(angle)),
                 -0.5,
-                # -5,
-                # 0 - 10 * (1 - abs(box[1] - box[3]))
-                5 - 20 * depth / 1000
+                # -5, # No depth.
+                # 0 - 10 * (1 - abs(box[1] - box[3])) # Rough relative depth.
+                10 + depth # MiDaS relative depth.
             )
             cube()
             glPopMatrix()
 
-    # Rotate the cube
-    # glPushMatrix()
-    # glRotatef(angle, 0, 1, 0)  # Rotate around the y-axis
-    # cube()
-    # glPopMatrix()
-
-    # glPushMatrix()
-    # glTranslatef(5, -0.5, -5)
-    # cube()
-    # glPopMatrix()
-
     glutSwapBuffers()
-
-    # angle += 1  # Increment the angle for the next frame
 
 def update(value):
     glutPostRedisplay()
-    glutTimerFunc(int(1000/60), update, 0)
+    glutTimerFunc(1000 // 60, update, 0)
 
 def main():
     glutInit()
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
     glutInitWindowSize(WIDTH, HEIGHT)
-    glutCreateWindow("Rotating Cube")
+    glutCreateWindow("3D Interface")
     glutDisplayFunc(display)
     glClearColor(1.0, 1.0, 1.0, 1.0)
-    glutTimerFunc(int(1000/60), update, 0)
+    glutTimerFunc(1000 // 60, update, 0)
     glEnable(GL_DEPTH_TEST)
     glutMainLoop()
 
