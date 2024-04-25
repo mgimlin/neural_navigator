9d5065e24d6db80c95f3d9309f917ad004ae98d9
nathanm1034 <nathanm285@gmail.com>
2024-04-24 23:36:23 -0500

    testing scale

--- 3D_interface/temp.py
+++ 3D_interface/temp.py
@@ -3,28 +3,36 @@ from OpenGL.GLUT import *
 from OpenGL.GLU import *
 
 import sys
+import time
+import threading
 
 import models
+
+TEMP_BACKWARD_STEPS = 5
+
+DELTA_Z = 0.0
 
 # Globals for the window dimensions
 WIDTH = 800
 HEIGHT = 600
 
 def display() -> None:
+    global DELTA_Z
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
-    glScale(0.25, 0.25, 0.25)
+    
+    glScalef(0.25, 0.25, 0.25)
     glTranslatef(0.0, -0.5, 0)
     glRotatef(45, 1, 0, 0)
     
     glPushMatrix()
-    models.draw_model(0)
-    
-    modelview = glGetDoublev(GL_MODELVIEW_MATRIX)
-    projection = glGetDoublev(GL_PROJECTION_MATRIX)
-    viewport = glGetIntegerv(GL_VIEWPORT)
-    gluProject(0, 1, 0, modelview, projection, viewport)
+    z = 0 - DELTA_Z
+    print(z)
+    # glTranslate(0, 0, z)
+    models.draw_model(0, z)
+
     glPopMatrix()
+    time.sleep(1)
             
     glutSwapBuffers()
 
@@ -36,6 +44,14 @@ def update(value: int) -> None:
     """
     glutPostRedisplay()
     glutTimerFunc(1000 // 60, update, 0)
+    
+def move_backwards():
+    global DELTA_Z
+    
+    for i in range(40):
+        DELTA_Z += 1
+        glutPostRedisplay()
+        time.sleep(1)
 
 def main() -> None:
     # Setup OpenGL and run the display loop.
@@ -50,6 +66,7 @@ def main() -> None:
     gluPerspective(90, WIDTH / HEIGHT, 0.1, 50.0)
     glClearColor(1.0, 1.0, 1.0, 1.0)
     glEnable(GL_DEPTH_TEST)
+    threading.Thread(target=move_backwards).start()
     glutMainLoop()
 
 if __name__ == "__main__":
